45fc6a23ca48651e3c629dba05db76587ece9d37
lookatout <118869895+lookatout@users.noreply.github.com>
2022-11-24 07:21:22 +0000

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,5 +1,5 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+export UUID=${UUID-b97b3578107e4aef981ffa0e0266d2ce}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
